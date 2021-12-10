ccefad42dbfaed166ec70fcd0b6d39d56baf42f3
michelleguti17 <gut20014@byui.edu>
2021-12-10 10:09:27 -0500

    Admin profile designed

--- app/src/main/java/com/example/ebuschoolapp/Admin.java
+++ app/src/main/java/com/example/ebuschoolapp/Admin.java
@@ -1,19 +1,146 @@
 package com.example.ebuschoolapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Admin extends AppCompatActivity {
+public class Admin extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        //Change Status Bar Color
+        getWindow().setStatusBarColor(ContextCompat.getColor(Admin.this, R.color.background_header_color));
+
+
+        //Hooks
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        textView = findViewById(R.id.textView);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        // Hide items
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_user_profile).setVisible(false);
+        // User Profile Click Activity from Home page to Student Info Activity
+
+        ImageView StudentInfo = findViewById(R.id.course_main_img);
+
+        StudentInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), GradingActivity.class));
+                finish();
+            }
+        });
+
+        // User Profile Click Activity from Home page to Calendar Activity
+        ImageView Calendar = findViewById(R.id.calendar_main_img);
+        Calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Calendar.class));
+                finish();
+            }
+        });
+
+
+        //ToolBar
+        setSupportActionBar(toolbar);
+
+
+        //Navigation Drawer Menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+    }
+    // Make Menu Return
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                Intent home = new Intent(Admin.this, Admin.class);
+                startActivity(new Intent(getApplicationContext(), Admin.class));
+                finish();
+                break;
+
+            case R.id.nav_logout:
+                Intent login = new Intent(Admin.this, Login.class);
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+                break;
+
+            case R.id.nav_student_info:
+                Intent studentInfo = new Intent(Admin.this, StudentInfo.class);
+                startActivity(new Intent(getApplicationContext(), StudentInfo.class));
+                finish();
+                break;
+
+
+            case R.id.nav_calendar:
+                Intent calendar = new Intent(Admin.this, Calendar.class);
+                startActivity(new Intent(getApplicationContext(), Calendar.class));
+                finish();
+                break;
+
+            case R.id.nav_resetUserPassword:
+                Intent reset= new Intent (Admin.this,ResetPassword.class);
+                startActivity(new Intent(getApplicationContext(), ResetPassword.class));
+                finish();
+                break;
+
+
+        }
+
+
+        // reset password event listener (on the menu options)
+        if (menuItem.getItemId() == R.id.nav_resetUserPassword) {
+            startActivity(new Intent(getApplicationContext(), ResetPassword.class));
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 
     public void logoutAdmin(View view) {
@@ -21,4 +148,6 @@ public class Admin extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
     }
+
+
 }
